1c3ec05feba36464a292d256a17a7f87578124c0
Tom Banish <tombanish@gmail.com>
2019-11-18 03:40:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
 def turn_count(board)
+  number_of_turns = 0
   board.each do |token|
     if token == "X" || token == "O"
     number_of_turns += 1
