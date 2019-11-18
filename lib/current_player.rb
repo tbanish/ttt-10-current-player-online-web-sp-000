986651965ccc9bbde04b6fe80fd16b0ed4ba34a0
Tom Banish <tombanish@gmail.com>
2019-11-18 03:31:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,5 +2,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 number_of_turns = 0
 
 def turn_count(board)
-  board.each do{
+  board.each do |turn|
+    puts "Turn number: #{number_of_turns}"
+    number_of_turns += 1
+  end
 end
